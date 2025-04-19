37fcea1391ce8fffa1178bb70adecf42fb1c4317
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2025-04-19 11:53:48 +0200

    Stripe

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,6 +7,20 @@ module.exports = defineConfig({
     {
       resolve: "./src/modules/brand",
     },
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+            },
+          },
+        ],
+      },
+    },
   ],
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
